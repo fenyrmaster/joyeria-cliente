12f3671bc6a6517c275e35e4bb15c4ccc859b329
fenyrmaster <brandony2004@gmail.com>
2021-12-11 10:39:54 -0600

    Mejorado las subcategorais

--- controllers/subcategoriasController.js
+++ controllers/subcategoriasController.js
@@ -1,4 +1,5 @@
 const Subcategoria = require("../models/subcategoriaModel");
+const Producto = require("../models/productoModel");
 const catchAsync = require("../utils/catchAsync");
 const ApiErrors = require("../utils/appError");
 const factory = require("../controllers/handlerFactory");
@@ -19,4 +20,18 @@ exports.createSubcategoria = catchAsync(async(req,res) => {
 exports.getAllSubs = factory.getAll(Subcategoria);
 exports.getSub = factory.getOne(Subcategoria);
 exports.updateSub = factory.updateData(Subcategoria);
-exports.removeSub = factory.deleteOne(Subcategoria);
+exports.removeSub = catchAsync(async (req,res,next) => {
+    const productos = await Producto.find({tipo: {$ne: "Catalogo"}, subcategoria: req.params.id});
+    if(productos.length === 0){
+        const doc = await Subcategoria.findByIdAndDelete(req.params.id);
+        if(!doc) {
+            return next(new ApiErrors(`The document with this id (${req.params.id}) doesnt exist`, 404))
+        }
+        res.status(204).json({
+            status: "success",
+            data: null
+        })
+    } else{
+        return next(new ApiErrors(`Hay ${productos.length} productos que tienen esta subcategoria, elimine o cambie la subcategoria de esos productos en "Administrar Productos"`, 400))
+    }
+});
